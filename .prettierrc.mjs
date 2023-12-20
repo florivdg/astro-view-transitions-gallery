e06e53c909fdd124e744847c6688099335ecf32f
Florian van der Galiën <florian.vandergalien@tmt.de>
2023-12-20 15:47:46 +0100

    Basic layout and routing

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  plugins: ['prettier-plugin-astro'],
+}
